ea636b75e59eb278543d51a780b3445c3460e18f
Ivan Poluianov <ivan.poluianov@gmail.com>
2024-01-20 10:02:30 +0100

    Update generate_keys.go

--- api/bitcoin/generate_keys.go
+++ api/bitcoin/generate_keys.go
@@ -8,6 +8,7 @@ import (
 	"github.com/btcsuite/btcd/btcec/v2"
 	"github.com/btcsuite/btcd/btcutil"
 	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/btcsuite/btcutil/bech32"
 	"github.com/ipoluianov/rusty/utils"
 )
 
@@ -19,6 +20,7 @@ func GenerateKeys(w http.ResponseWriter, r *http.Request) {
 		PublicKeyUncompressed string `json:"public_key_uncompressed"`
 		AddressP2PKHHex       string `json:"address_p2pkh_hex"`
 		AddressP2PKH          string `json:"address_p2pkh"`
+		AddressBech32         string `json:"address_bech32"`
 	}
 
 	privateKey, err := btcec.NewPrivateKey()
@@ -51,6 +53,12 @@ func GenerateKeys(w http.ResponseWriter, r *http.Request) {
 		log.Fatal(err)
 	}
 
+	witnessProgram := btcutil.Hash160(serializedPubKeyCompressed)
+	bech32Address, err := bech32.Encode("bc", append([]byte{0x00}, witnessProgram...))
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	var res Result
 	res.PrivateKeyCom = privateKeyWIFCom.String()
 	res.PrivateKeyUncom = privateKeyWIFUnCom.String()
@@ -58,6 +66,7 @@ func GenerateKeys(w http.ResponseWriter, r *http.Request) {
 	res.PublicKeyUncompressed = serializedPubKeyUncompressedHex
 	res.AddressP2PKHHex = p2pkhAddress.String()
 	res.AddressP2PKH = p2pkhAddress.EncodeAddress()
+	res.AddressBech32 = bech32Address
 
 	utils.SendJson(w, res, nil)
 }
