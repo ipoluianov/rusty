225f8a6b2913af7ceca48abc27d8d48bdb3213bd
Ivan Poluianov <ivan.poluianov@gmail.com>
2024-01-20 09:39:38 +0100

    Update generate_keys.go

--- api/bitcoin/generate_keys.go
+++ api/bitcoin/generate_keys.go
@@ -1,13 +1,12 @@
 package bitcoin
 
 import (
-	"encoding/hex"
 	"log"
 	"net/http"
 
 	"github.com/btcsuite/btcd/btcec/v2"
 	"github.com/btcsuite/btcd/btcutil"
-	"github.com/btcsuite/btcutil/bech32"
+	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/ipoluianov/rusty/utils"
 )
 
@@ -18,34 +17,20 @@ func GenerateKeys(w http.ResponseWriter, r *http.Request) {
 		Address    string `json:"address"`
 	}
 
-	// Генерация приватного ключа
 	privateKey, err := btcec.NewPrivateKey()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Получение публичного ключа из приватного
-	publicKey := privateKey.PubKey()
-
-	// Генерация Bitcoin-адреса
-	/*address, err := btcutil.NewAddressPubKey(publicKey.SerializeCompressed(), &chaincfg.MainNetParams)
+	privateKeyWIF, err := btcutil.NewWIF(privateKey, &chaincfg.MainNetParams, false)
 	if err != nil {
 		log.Fatal(err)
-	}*/
-
-	witnessProgram := btcutil.Hash160(publicKey.SerializeUncompressed())
-
-	// Кодирование в Bech32
-	bech32Address, err := bech32.Encode("bc", append([]byte{0x00}, witnessProgram...))
-	if err != nil {
-		utils.SendError(w, err)
-		return
 	}
 
 	var res Result
-	res.PrivateKey = hex.EncodeToString(privateKey.Serialize())
-	res.PublicKey = hex.EncodeToString(publicKey.SerializeUncompressed())
-	res.Address = bech32Address
+	res.PrivateKey = privateKeyWIF.String()
+	res.PublicKey = ""
+	res.Address = ""
 
 	utils.SendJson(w, res, nil)
 }
